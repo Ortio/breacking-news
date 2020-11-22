13170dc23e65be29df88e3ad77744b0997510e52
Murad <ortio@mail.ru>
2020-11-22 20:38:17 +0300

    delete excess tag

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -4,14 +4,11 @@
     <div class="row">
         <h1>Главные новости!</h1>
     @foreach($data as $item)
-        <div class="news-block">
-            <div class="col-md-12">
+            <div class="news-block col-md-12">
                 <h3><a href="/news/{{$item->id}}">{{$item->title}}</a></h3>
                 <p>{{ Str::limit(html_entity_decode($item->text), 200)}}</p>
                 <a type="button" class="btn btn-primary" href="/news/{{$item->id}}">Подробнее</a>
             </div>
-        </div>
-
     @endforeach
     </div>
 @endsection
